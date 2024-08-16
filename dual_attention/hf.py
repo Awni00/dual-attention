9a203fa3f46319d4ad655d92055b1fe04ba8c9b2
Awni <awnyaltabaa@gmail.com>
2024-08-15 21:08:17 -0400

    added package code

---
+++ dual_attention/hf.py
@@ -0,0 +1,6 @@
+from huggingface_hub import PyTorchModelHubMixin
+from dual_attention.language_models import DualAttnTransformerLM
+
+class DualAttnTransformerLM_HFHub(PyTorchModelHubMixin, DualAttnTransformerLM):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
