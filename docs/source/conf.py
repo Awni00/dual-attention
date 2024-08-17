ba40ea33a426f389ce87bebfbe3010b618eb9807
Awni <awnyaltabaa@gmail.com>
2024-08-17 17:10:01 -0400

    updated docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,10 +29,12 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-html_theme = 'sphinx_book_theme'
+# html_theme = 'sphinx_book_theme'
 # html_theme = 'sphinx_rtd_theme'
-# html_theme = 'pydata_sphinx_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+html_permalinks_icon = '§'
 
 autodoc_default_options = {
     'members': True,
