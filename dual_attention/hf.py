ec3b57194aa96c23e0929ee18c99e409d45a73e7
Awni00 <awnyaltabaa@gmail.com>
2024-09-24 15:51:39 -0400

    added Transformer LM HFHub Mixin for loading Transformer baselines

--- dual_attention/hf.py
+++ dual_attention/hf.py
@@ -3,7 +3,7 @@ This module implements a Huggingface Hub mixin for the Dual Attention Transforme
 """
 
 from huggingface_hub import PyTorchModelHubMixin
-from dual_attention.language_models import DualAttnTransformerLM
+from dual_attention.language_models import DualAttnTransformerLM, TransformerLM
 
 class DualAttnTransformerLM_HFHub(PyTorchModelHubMixin, DualAttnTransformerLM):
     """
@@ -20,5 +20,24 @@ class DualAttnTransformerLM_HFHub(PyTorchModelHubMixin, DualAttnTransformerLM):
     ```
     """
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class TransformerLM_HFHub(PyTorchModelHubMixin, TransformerLM):
+    """
+    Huggingface Hub mixin for the Transformer Language Model.
+
+    Used for the purposes of loading baselines trained via the same procedure as DAT models.
+
+    This class inherits from both the PyTorchModelHubMixin and the TransformerLM classes.
+    It allows you to load a DAT model from Huggingface Hub using the `from_pretrained` method.
+
+    Example:
+    ```python
+    from dual_attention.hf import TransformerLM_HFHub
+
+    model = TransformerLM_HFHub.from_pretrained("awni00/T-sa24-757M")
+    ```
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
